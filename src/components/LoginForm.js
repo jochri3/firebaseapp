1b0de96566c8d992492a6ab75c93cf7ea2e3dcdc
Christian Lisangola <christian.lisangola@gmail.com>
2019-05-18 12:58:34 +0100

    TextInput

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,32 @@
 import React,{ Component } from 'react';
-import { View } from 'react-native';
+import { TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      text:''
+    }
+  }
+  //Le text input par defaut a une width:0 et height:0,donc par defaut il est invisible
+  //La propriete "value" n'a pas de valeur lorsque le component s'affiche,mais une fois
+  //  que l'utilisateur saisie quelque chose,le composant se reaffiche,et mets dans "value" la valeur contenu dans le state
   render(){
     return (
       <Card>
-        <CardSection></CardSection>
-        <CardSection></CardSection>
+        <CardSection>
+          <TextInput 
+            value={ this.state.state }
+            style={{ height:20,width:100 }} 
+            onChangeText={text=>this.setState({ text })}
+            />
+        </CardSection>
+
+        <CardSection>
+
+        </CardSection>
+
         <CardSection>
           <Button>
             Log in
