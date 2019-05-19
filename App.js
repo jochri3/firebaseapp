630f833864e8bd72c399ed9bcbd049de7a623662
Christian Lisangola <christian.lisangola@gmail.com>
2019-05-19 10:11:51 +0100

    Dynamically render FormLogin or Logout button according to use signin status

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React,{ Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './src/components/common/';
+import { Header, Button } from './src/components/common/';
 import LoginForm from "./src/components/LoginForm";
 import { CONFIG } from './src/config';
 
 
 class App extends Component{
+  state={
+    loggedIn:false
+  }
+
   componentWillMount(){
     const { apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId } = CONFIG;
     firebase.initializeApp(
@@ -19,13 +23,37 @@ class App extends Component{
               messagingSenderId
           }
       );
+
+      //This fat arrow function will be called whenever user signs in or signs out from firebase
+      //this "user" argument will we a object when the use is signed in,and will be "null" when is not
+      firebase.auth().onAuthStateChanged((user)=>{
+        //We've putted this logic here so that the <App /> component which is the parent component can use "loggedIn" ou "loggedOut" state
+        //  to decides whetheer it should show or not the  Loggin form
+        if(user){
+          this.setState({loggedIn:true});
+        }else{
+          this.setState({loggedIn:false});
+        }
+      });
+  }
+
+  onButtonPress(){
+    this.setState({loggedIn:false});
+  }
+
+  renderContent(){
+    return this.state.loggedIn?
+     <Button>
+            Log Out
+      </Button>
+    :<LoginForm />;
   }
 
   render(){
     return (
       <View>
         <Header title="Authentication" />
-        <LoginForm />
+        { this.renderContent() }
       </View>
     );
   }
